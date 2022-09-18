89c1d4a964a270e690b179d94c210c700ab6f4ea
Jarraozinho <84052280+Jarraozinho@users.noreply.github.com>
2022-09-18 11:38:44 -0300

    Create mainpilha.cpp

---
+++ mainpilha.cpp
@@ -0,0 +1,36 @@
+  #include <iostream>
+#include "pilha.h"
+using namespace std;
+
+int main(){
+    pilha pilha1;
+    Tipoitem item;
+    int opcao;
+    cout << "Programa gerador de pilha :" << endl;
+
+    do {
+        cout << "Janeiro " << endl;
+        cout << "Fevereiro" << endl;
+        cout << "Marco" << endl;
+        cout << "Abril" << endl;
+        cout << "Maio " << endl;
+        cout << "Junho " << endl;
+        cout << "Julho " << endl;
+        cout << "Agosto " << endl;
+        cout << "Setembro " << endl;
+        cout << "Outubro " << endl;
+        cout << "Novembro " << endl;
+        cout << "Dezembro "  << endl;
+
+        cin >> opcao;
+        if (opcao == 1){
+            cout << ""
+        }
+
+    }
+
+
+    return 0;
+
+
+}
